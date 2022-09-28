b3aeec41fbc61cfab95978c08ee600e338397f0e
Uíliam Mateu Martins <100007488.joan23@fje.edu>
2022-09-28 20:39:55 +0200

    Subida 28/09

--- Encriptacion1.php
+++ Encriptacion1.php
@@ -1,6 +1,49 @@
 <?php
-// const VOCALES = ['a','e','i','o','u','A','E','I','O','U'];
 const VOCALES = ['a','e','i','o','u'];
-$msgE1 = str_split($_POST['msgE1']);
+// ENCRIPTACION 1 A ORIGINAL
+$txtCon = [];
+$inicio = [];
+if (empty($_POST['msgE1'])) {
+    $msgE1 = str_split($msgDE2);
+} else {
+    $msgE1 = str_split($_POST['msgE1']);
+}
+// E nul nugad rel aM ahcna...
+$j = 0;
 
-// 
+
+
+for ($i=0; $i < count($msgE1) ; $i++) {
+
+    if ($i == 0){
+        if (in_array(strtolower($msgE1[$i]), VOCALES)) {
+            // array_push($txtVo, $msgE1[$i]);
+            array_push($inicio, $msgE1[$i]);
+        } else {
+            array_push($inicio, $msgE1[$i]);
+        }
+
+    } else {
+
+        if (in_array(strtolower($msgE1[$i]), VOCALES) != false) {
+            array_push($inicio, $msgE1[$i]);
+            array_push($txtVo, $msgE1[$i]);
+        } else {
+
+            while (in_array(strtolower($msgE1[$i]), VOCALES) == false) {
+                array_push($txtCon, $msgE1[$i]);
+                $i++;
+                if ($i >= count($msgE1)) {
+                    break;
+                }
+            }
+            $txtCon = array_reverse($txtCon);
+            $inicio = array_merge($inicio, $txtCon);
+            array_push($inicio, $msgE1[$i]);
+            $txtCon = [];
+            
+        }
+    }
+
+}
+$msgDE1 = implode($inicio);
--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mensaje encriptado</title>
     <?php error_reporting(0);
      require_once './Encriptacion1.php'; ?>
@@ -14,8 +12,9 @@
             <legend>Mensaje</legend>
             Mensaje encriptado2: <textarea name="msgE2" id="" cols="30" rows="10"></textarea> <br><br>
             <!-- Mensaje encriptado1: <input type="text" name="msgE1" value="" styles="width:100%;"><br><br> -->
-            Mensaje encriptado1: <textarea name="msgE1" id="" cols="30" rows="10"><?php echo $msgDE2; ?></textarea><br><br>
-            Mensaje original: <input type="text" name="msgO"><br><br>
+            Mensaje encriptado1: <textarea name="msgE1" id="" cols="30" rows="10"></textarea><br><br>
+            Mensaje original: <textarea name="msgO" id="" cols="30" rows="10"><?php echo $msgDE1; ?></textarea> <br><br>
+
             <br>
             <input type="submit" value="Desencriptar">
         </fieldset>
