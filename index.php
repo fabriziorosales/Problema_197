a375d6d628d5a25008418721a61dded5f4cc8829
Uíliam Mateu Martins <100007488.joan23@fje.edu>
2022-09-28 17:40:34 +0200

    Subida 28/09

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link> </link>
+    <title>Problema 197</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text">
+        <input type="" name="" id="">
+
+    </form>
+
+</body>
+</html>
+
+<?php
+
+
+
