1879f6bb3c14b878d8985f2813ed5223f86a7109
Uíliam Mateu Martins <100007488.joan23@fje.edu>
2022-09-28 19:36:59 +0200

    Subida 28/09

---
+++ Encriptacion1.php
@@ -0,0 +1,6 @@
+<?php
+// const VOCALES = ['a','e','i','o','u','A','E','I','O','U'];
+const VOCALES = ['a','e','i','o','u'];
+$msgE1 = str_split($_POST['msgE1']);
+
+// 
--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
-    <link> </link>
-    <title>Problema 197</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mensaje encriptado</title>
+    <?php error_reporting(0);
+     require_once './Encriptacion1.php'; ?>
 </head>
 <body>
-    <form action="" method="post">
-        <input type="text">
-        <input type="" name="" id="">
-
+    <form action="./index.php" method="POST">
+        <fieldset>
+            <legend>Mensaje</legend>
+            Mensaje encriptado2: <textarea name="msgE2" id="" cols="30" rows="10"></textarea> <br><br>
+            <!-- Mensaje encriptado1: <input type="text" name="msgE1" value="" styles="width:100%;"><br><br> -->
+            Mensaje encriptado1: <textarea name="msgE1" id="" cols="30" rows="10"><?php echo $msgDE2; ?></textarea><br><br>
+            Mensaje original: <input type="text" name="msgO"><br><br>
+            <br>
+            <input type="submit" value="Desencriptar">
+        </fieldset>
     </form>
-
 </body>
 </html>
-
-<?php
-
-
-
